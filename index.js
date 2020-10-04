fc76d896880930f48bb89d21616927aa2e4fabc0
tayab-pabel <tayabpabel@yahoo.com>
2020-10-04 14:20:09 +0600

    root api created

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.use(cors());
 
 const port = 5000;
 
+app.get('/', (req, res) =>{
+  res.send("It's Working")
+})
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
 client.connect(err => {
   const productsCollection = client.db("emaJhonStore").collection("products");
